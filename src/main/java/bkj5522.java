b8eaa2cdff0babd4157f49bf90a3734fb66c4250
Chokkachi <csu0915@gmail.com>
2023-04-12 00:47:55 +0900

    0412 퀴즈 버퍼리더로

--- src/main/java/bkj5522.java
+++ src/main/java/bkj5522.java
@@ -1,17 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class bkj5522 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        Scanner sc = new Scanner(System.in);
+//        sc = new Scanner(sc.nextLine()).useDelimiter(",\\s*");
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(),", ");
 
-        int sum=0;
+        int arr[] = new int[5];
 
-        for (int i = 0; i < 5; i++) {
-            sum += Integer.parseInt(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
         }
 
-        System.out.println(sum);
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("가장 큰 값 : " + max);
+        System.out.println("가장 작은 값 : " + min);
+        System.out.println("평균 값 : " + sum / (float) arr.length);
     }
 }
