4e40c4b7d3b1927806fd7650699dbde07023a27a
Chokkachi <csu0915@gmail.com>
2023-04-22 23:00:11 +0900

    0422 백준27866

---
+++ src/main/java/bkj27866.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bkj27866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine().trim();
+        int i = Integer.parseInt(br.readLine());
+
+        System.out.print(str.substring(i - 1, i));
+    }
+}
