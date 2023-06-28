a357a25d7d8c56d48cd2050eb9d88569695a88e0
SeongukCho <tjddnr377@naver.com>
2023-06-28 22:52:47 +0900

    0628

---
+++ src/main/java/prog181914.java
@@ -0,0 +1,11 @@
+public class prog181914 {
+    public int solution(String number) {
+        int answer = 0;
+        String[] str = number.split("");
+        for (int i = 0; i < str.length; i++){
+            answer += Integer.parseInt(str[i]);
+        }
+        answer = answer%9;
+        return answer;
+    }
+}
