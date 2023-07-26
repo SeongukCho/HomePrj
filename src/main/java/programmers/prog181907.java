095474e7ee98d3916a825a9e375f2526fdeb510a
SeongukCho <tjddnr377@naver.com>
2023-07-26 22:54:04 +0900

    0726 잔디

---
+++ src/main/java/programmers/prog181907.java
@@ -0,0 +1,8 @@
+package programmers;
+
+public class prog181907 {
+    public String solution(String my_string, int n) {
+        String answer = my_string.substring(0,n);
+        return answer;
+    }
+}
