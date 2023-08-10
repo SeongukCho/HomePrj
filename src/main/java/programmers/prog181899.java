0e8a30b296da760ba9c4f496f0eddad939018699
SeongukCho <tjddnr377@naver.com>
2023-08-10 22:25:16 +0900

    0810 잔디

---
+++ src/main/java/programmers/prog181899.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class prog181899 {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[start-end+1];
+        for (int i = 0; i < answer.length; i++){
+            answer[i]=start-i;
+        }
+        return answer;
+    }
+}
