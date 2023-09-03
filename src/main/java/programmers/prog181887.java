ac0a67acdb9658491030fb3224c35ead37832216
SeongukCho <tjddnr377@naver.com>
2023-09-03 23:29:09 +0900

    잔디

---
+++ src/main/java/programmers/prog181887.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class prog181887 {
+    public int solution(int[] num_list) {
+        int evensum = 0;
+        int oddsum = 0;
+        for (int i = 0; i < num_list.length; i++){
+            if(i%2 == 0){
+                oddsum += num_list[i];
+            } else{
+                evensum += num_list[i];
+            }
+        }
+        return evensum > oddsum ? evensum : oddsum;
+    }
+}
